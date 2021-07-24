9be6abdde8e9bc8c10b5070f8912b6710ebc67c1
flyshx <87915639+flyshx@users.noreply.github.com>
2021-07-24 23:43:43 +0200

    Add files via upload

---
+++ WindowsDefenderControl/Form1.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsDefenderControl
+{
+    public partial class flyshx : Form
+    {
+        public flyshx()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        public static void refresh()
+        {
+            
+        }
+        private void flyshx_Load(object sender, EventArgs e)
+        {
+            RegistryKey keyA = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows Defender", true);
+            if (keyA.GetValue("DisableAntiSpyware").ToString() == "0")
+            {
+                label1.Text = "Windows Defender is enabled (0)";
+            }
+            else if (keyA.GetValue("DisableAntiSpyware").ToString() == "1")
+            {
+                label1.Text = "Windows Defender is disabled (1)";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            RegistryKey keyA = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows Defender", true);
+            if (keyA.GetValue("DisableAntiSpyware").ToString() == "0")
+            {
+                label1.Text = "Windows Defender is enabled (0)";
+            }
+            else if (keyA.GetValue("DisableAntiSpyware").ToString() == "1")
+            {
+                label1.Text = "Windows Defender is disabled (1)";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RegistryKey keyA = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows Defender", true);
+            keyA.SetValue("DisableAntiSpyware", 1);
+            if (keyA.GetValue("DisableAntiSpyware").ToString() == "0")
+            {
+                label1.Text = "Failed to enable Windows Defender (0)";
+            }
+            else if (keyA.GetValue("DisableAntiSpyware").ToString() == "1")
+            {
+                label1.Text = "Disabled Windows Defender (1)";
+            }
+            keyA.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RegistryKey keyA = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows Defender", true);
+            keyA.SetValue("DisableAntiSpyware", 0);
+            if (keyA.GetValue("DisableAntiSpyware").ToString() == "0")
+            {
+                label1.Text = "Enabled Windows Defender (0)";
+            }
+            else if (keyA.GetValue("DisableAntiSpyware").ToString() == "1")
+            {
+                label1.Text = "Failed to enable Windows Defender (1)";
+            }
+            keyA.Dispose();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Contact me on Discord: moodless#0001");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The number displays the registery value","Flyshx");
+        }
+    }
+}
